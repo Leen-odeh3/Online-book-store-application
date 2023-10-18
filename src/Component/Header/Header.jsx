27115a708aa685a078404400188c02a9e68bc48e
leen <leenodeh287@gmail.com>
2023-10-18 17:15:32 +0300

    add public style

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="top-header-phone">
+        <i className="bi bi-telephone-fill"></i>
+        123-456-789 101
+      </div>
+      <div className="top-header-title">Welcome To Online Book Store </div>
+      <div className="top-header-login">
+        <i class="bi bi-person-fill"></i>
+        Login
+      </div>
+    </div>
+  );
+};
+
+export default Header;
