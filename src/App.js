db3aeb0669d33749a7f06f490a61c710a493e3ae
leen <leenodeh287@gmail.com>
2023-10-20 15:16:34 +0300

    add routing links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Author from "./Pages/Authors/Author";
 import AboutUs from "./Pages/AboutUs/AboutUs";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Cart from '../src/Pages/Cart/Cart.jsx'
+import Login from '../src/Forms/Login';
+import Register from '../src/Forms/Register'
+import Book from '../src/Pages/Book'
+
 
 function App() {
   return (
@@ -13,6 +19,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/authors" element={<Author/>} />
         <Route path="/aboutus" element={<AboutUs/>} />
+        <Route path="/contactUs" element={<ContactUs/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/book/:id" element={<Book/>} />
+        <Route path="/login" element={<Login/>} />
+        
 
       </Routes>
       <Footer />
