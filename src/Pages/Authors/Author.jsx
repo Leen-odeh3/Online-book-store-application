0d0a099884e9c8eaefd81c8401474fe00b6f7007
leen <leenodeh287@gmail.com>
2023-10-21 19:59:05 +0300

    add structure authors page

--- src/Pages/Authors/Author.jsx
+++ src/Pages/Authors/Author.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Authors.css'
+import {authors} from '../../data/authors'
 
 const Author = () => {
   return (
-    <div>
-      authors
+    <div className='author-content'>
+      <div className='search-author'><input type="search" placeholder='search in authors' className='search-authors'/> </div>
+      <div className='authors-wrapper'> 
+        {authors.map((e) => {
+          return (
+            <div className='auther-item' key={e.id}>
+              <img src={e.image} alt={e.name}  />
+              <h4>{e.name}</h4>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
 
-export default Author
+export default Author;
