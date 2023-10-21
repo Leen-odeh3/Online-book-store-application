9bca672ab0812f5631ec3163b5787ef94a74703e
leen <leenodeh287@gmail.com>
2023-10-21 20:18:54 +0300

    add styles pages

--- src/Forms/Login/Login.jsx
+++ src/Forms/Login/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./forms.css";
+import './../Register/forms.css';
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
